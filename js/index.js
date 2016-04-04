2f3e07466ce85830759eb411e67ec1e1f9b7661c
Cherylyli <cherylliat@gmail.com>
2016-04-04 00:40:30 -0400

    second view
    completed js for the second view of the landing page. font still needs
    changing

--- js/index.js
+++ js/index.js
@@ -1,4 +1,16 @@
-$(".what-it-is, .how-it-works").on("click", function(){
-	$(this).toggleClass("selected");
-})
+//toggle between what is shown: either what-it-is or how-it-works
+$(".what-it-is").on("click", function(){
+	$(this).addClass("selected");
+	$(this).siblings().removeClass("selected");
+	$(this).parent().nextAll(".how-it-works").fadeOut(200, function(){
+		$(this).siblings().fadeIn(300);
+	});
+});
 
+$(".how-it-works").on("click", function(){
+	$(this).addClass("selected");
+	$(this).siblings().removeClass("selected");
+	$(this).parent().nextAll(".what-it-is").fadeOut(200, function(){
+		$(this).siblings().fadeIn(300);
+	});
+});
